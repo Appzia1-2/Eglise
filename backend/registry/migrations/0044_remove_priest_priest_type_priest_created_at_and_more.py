2d561b21d78b51b1ffc8b0f0bd6dfeed2adfd2fe
kevin chacko <kevinappzia@gmail.com>
2026-07-29 19:48:11 +0530

    Add frontend and backend changes

--- backend/registry/migrations/0044_remove_priest_priest_type_priest_created_at_and_more.py
+++ backend/registry/migrations/0044_remove_priest_priest_type_priest_created_at_and_more.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='priest',
             name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=1),
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
             preserve_default=False,
         ),
         migrations.AddField(
